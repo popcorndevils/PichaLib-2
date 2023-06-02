7824c685957f0c4a02057354bc17bfc160dfae5e
Michael Coates <popcorndevils@gmail.com>
2023-06-02 09:30:13 -0400

    simplify picha model

--- src/types/Canvas.cs
+++ src/types/Canvas.cs
@@ -3,7 +3,7 @@
 namespace PichaLib2.Types
 {
     /// <summary>
-    /// Canvases are a collection of layers and represent the 'world space' where layers are placed in a sprite. Canvases use animations to represent which zoetrope in each layer 'play' at the same time.
+    /// Canvases are a collection of layers and represent the 'world space' where layers are placed in a sprite.
     /// </summary>
     public class Canvas
     {
--- src/types/Layer.cs
+++ src/types/Layer.cs
@@ -3,7 +3,7 @@
 namespace PichaLib2.Types
 {
     /// <summary>
-    /// Layers are a sub-component of the Canvas, and represent a collection of 1 or more Zoetropes. Pixels and generation rules are defined at the Layer level.
+    /// Layers are a sub-component of the Canvas. Generation rules are defined at the Layer level.
     /// </summary>
     public class Layer
     {
